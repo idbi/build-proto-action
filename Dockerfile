f2ba70ae0e01c8a8d6553f274aa4cc58b379b67c
Nick Aguilar <nickaguilarh@gmail.com>
2022-12-15 17:08:55 -0500

    feat: initial commit
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM bufbuild/buf:latest
-
-COPY entrypoint.sh /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-set -e  # if a command fails it stops the execution
-set -u  # script fails if trying to access to an undefined variable
-
-echo "[+] Action start"
-
-buf --version
